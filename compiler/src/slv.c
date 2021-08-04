27342eb26052a2e0d9f2a289f06623fb08109417
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-04 09:22:25 -0400

    use compile cmd

--- compiler/src/slv.c
+++ compiler/src/slv.c
@@ -7,10 +7,20 @@ int main( int argc, char *argv[] )
 {
     createProjectFiles(argc,argv);
     checkForNoArgs(argc,argv);
+
+    // INIT
     if(strcmp(argv[2],"init") && argc==2){createInit();}
+
+    //CREATE
     if(strcmp(argv[2],"create")&&strcmp(argv[3],"new"), argc==4){
         char* path = argv[4];
-        compile(path);
+
     }
+    // COMPILE
+    if(strcmp(argv[2],"compile") && argc==3){
+        char* path = argv[3];
+        compile(path);
+    }   
+    
     return 0;
 }
