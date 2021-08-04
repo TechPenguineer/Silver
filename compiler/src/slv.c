4473b29c36bcfc5d3eaf8d471f161d3468c90669
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-03 23:33:02 -0400

    created create command

--- compiler/src/slv.c
+++ compiler/src/slv.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
-
-int main()
+#include <string.h>
+#include <stdbool.h>
+int main( int argc, char *argv[] )
 {
-    printf("Hello World!");
+    bool commandFound;
+    if(commandFound){}else{printf("SLV: Command not found");}
+    if(argc == 1)
+    {
+         commandFound=false;
+    }
+    // CREATE COMMAND
+    if(argv[1] == "create")
+    {
+        if (argc == 1)
+        {
+            printf("Not enough arguments supplied to execute this command");
+        }
+        
+        if(argv[2] == "new")
+        {
+
+            if(argc == 3)
+            {
+                 char projectname[50]; 
+                 strcpy(projectname, argv[3]);
+                 printf(projectname);
+            } else if(argc == 2)
+            {
+               printf("Not enough arguments supplied to execute this command");
+            }
+        }
+    } 
     return 0;
 }
